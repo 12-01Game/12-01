fd554d05c5b3bc2f0b6f64c43971323f0d1d0c13
Jonathan Ballands <jballands@gmail.com>
2014-04-14 01:41:17 -0400

    Added the beginnings of the character movement script

---
+++ 1201 Game/Assets/Scripts/CharacterScript.js
@@ -0,0 +1,92 @@
+﻿/*
+ *	CharacterScript.js
+ *
+ *	The script that causes a character to interact with the world.
+ *
+ *	Version 1.0
+ *	Coded with <3 by Jonathan Ballands
+ *
+ *	(C)2014 All Rights Reserved.
+ */
+
+#pragma strict
+@script RequireComponent(CharacterController)
+
+var jumpForce					: float = 10.0f;	// The amount of force that the character jumps with
+var speed						: float = 6.0f;		// The speed at which this character moves
+var rotateSpeed					: float = 150.0f;	// The speed at which this character rotates
+
+var canRotate					: boolean = true;	// Set this to true if you want the character to rotate
+var canJump						: boolean = true;	// Set this to true if you want the character to jump
+
+private var controller			: CharacterController;			// The CharacterController component that must be attached to this character
+
+private var faceVector			: Vector3 = Vector3.zero;		// The vector the character moves on
+private var downVector			: Vector3 = Vector3.down;		// The vector the character falls on
+private var savedXMotion		: float = 0;					// When the character is airborne, use this value to launch the character in this direction
+
+private var accelOfGravity		: float = .981;					// PHYSICS, BITCHES
+private var fallFrames			: int = 1;						// How long has this character been falling?
+
+private var shouldFaceAngle		: float = 0;
+private var currFaceAngle		: float = 0;
+
+/*
+ *	Awake()
+ *
+ *	Called when this script wakes up.
+ */
+function Awake() {
+	faceVector = transform.TransformDirection(Vector3.left);
+	controller = GetComponent("CharacterController");
+}
+
+/*
+ *	Update()
+ *
+ *	Called as this character updates.
+ */
+function Update () {
+	
+	var xMotion = 0;
+	var yMotion = 0;
+
+	// If the controller is grounded...
+	if (controller.isGrounded) {
+	
+		// Reset gravity acceleration
+		fallFrames = 1;
+		
+		// Get desired X-motion
+		xMotion = Input.GetAxis("Horizontal") * speed * -1;
+		savedXMotion = xMotion;
+		
+		// No Y-motion
+		yMotion = 0;
+		
+		// Set the angle to turn to
+		if (xMotion > 0) {
+			shouldFaceAngle = 0;
+		}
+		else if (xMotion < 0) {
+			shouldFaceAngle = 180;
+		}
+	}
+	else {
+		
+		// Simulate acceleration by multiplying by the number of frames the character has been airborne for
+		yMotion = accelOfGravity * fallFrames * -1;
+		fallFrames++;
+		
+		// Use the X-motion right before the character got airborne
+		xMotion = savedXMotion;
+	}
+	
+	// Determine rotation
+	if (currFaceAngle != shouldFaceAngle) {
+		transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+		currFaceAngle += rotateSpeed * Time.deltaTime;
+	}
+		
+	controller.Move(Vector3(xMotion * Time.deltaTime, yMotion * Time.deltaTime, 0));
+}
